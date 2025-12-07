ea68a67a0451fb50c2eb2f1d3957d4a8859bef65
Pentaminer <150195480+Pentaminer@users.noreply.github.com>
2025-12-07 22:46:36 +0200

    Update TestMod.java

--- src/main/java/com/example/testmod/TestMod.java
+++ src/main/java/com/example/testmod/TestMod.java
@@ -1,26 +1,34 @@
 package com.example.testmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.FoodComponents;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registries;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestMod implements ModInitializer {
-    public static final String MOD_ID = "test_mod";
+    public static final String MOD_ID = "test-mod";
+
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    // Одиночный Item
     public static final Item COOKIE_ITEM = new Item(new Item.Settings()
-        .group(ItemGroups.FOOD)
-        .food(FoodComponents.COOKIE)
+            .group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder()
+                    .hunger(4)
+                    .saturationModifier(0.6f)
+                    .build()
+            )
     );
 
     @Override
     public void onInitialize() {
-        Registries.register(Registries.ITEM, new Identifier(MOD_ID, "cookie_item"), COOKIE_ITEM);
+        // Регистрируем Item в классическом стиле
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "cookie_item"), COOKIE_ITEM);
+
         LOGGER.info("Hello Fabric world!");
     }
 }
