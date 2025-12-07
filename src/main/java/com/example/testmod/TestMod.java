7987b42c1a30909eacaccc631ffe013a51b7155b
Pentaminer <150195480+Pentaminer@users.noreply.github.com>
2025-12-07 20:56:26 +0200

    add cookie to testmod.java

--- src/main/java/com/example/testmod/TestMod.java
+++ src/main/java/com/example/testmod/TestMod.java
@@ -1,7 +1,11 @@
 package com.example.testmod;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,9 +16,17 @@ public class TestMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Item COOKIE_ITEM = new Item(new Item.Settings()
+        .group(ItemGroup.FOOD)
+        .food(new FoodComponent.Builder()
+            .hunger(4)       // сколько сытости даёт
+            .saturationModifier(0.6f) // насыщение
+            .build()
+        ));
 
 	@Override
 	public void onInitialize() {
+		Registry.register(Registry.ITEM, new Identifier("examplemod", "cookie_item"), COOKIE_ITEM);
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
