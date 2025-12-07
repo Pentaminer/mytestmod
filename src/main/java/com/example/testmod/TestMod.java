bd3aeb801327d2578723c052725de667a95f55f3
Pentaminer <150195480+Pentaminer@users.noreply.github.com>
2025-12-07 22:21:33 +0200

    Update TestMod.java

--- src/main/java/com/example/testmod/TestMod.java
+++ src/main/java/com/example/testmod/TestMod.java
@@ -1,36 +1,26 @@
 package com.example.testmod;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.FoodComponents;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.Registries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestMod implements ModInitializer {
-	public static final String MOD_ID = "test-mod";
+    public static final String MOD_ID = "test_mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final Item COOKIE_ITEM = new Item(new Item.Settings()
-        .group(ItemGroup.FOOD)
-        .food(new FoodComponent.Builder()
-            .hunger(4)       // сколько сытости даёт
-            .saturationModifier(0.6f) // насыщение
-            .build()
-        ));
+    public static final Item COOKIE_ITEM = new Item(new Item.Settings()
+        .group(ItemGroups.FOOD)
+        .food(FoodComponents.COOKIE)
+    );
 
-	@Override
-	public void onInitialize() {
-		Registry.register(Registry.ITEM, new Identifier("examplemod", "cookie_item"), COOKIE_ITEM);
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        Registries.register(Registries.ITEM, new Identifier(MOD_ID, "cookie_item"), COOKIE_ITEM);
+        LOGGER.info("Hello Fabric world!");
+    }
 }
